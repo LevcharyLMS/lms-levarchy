178166d4f08092e5bf62eb8ec25d12b6540a94d1
theeightboys <theboysofficialone@gmail.com>
2026-09-19 12:08:08 +0530

    fix netlify issue

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    serverComponentsExternalPackages: ['googleapis', 'resend', 'stripe'],
+    serverComponentsExternalPackages: ['googleapis', 'resend', 'stripe', 'pg'],
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
